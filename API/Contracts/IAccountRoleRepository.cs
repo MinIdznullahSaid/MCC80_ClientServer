eaee3a2774fe6ba62b43b57c3927d1d8ae731c9a
Min Idznullah Said <minidznullahsaid@gmail.com>
2023-07-20 19:27:38 +0700

    CRUD AccountRole

---
+++ API/Contracts/IAccountRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Contracts;
+
+public interface IAccountRoleRepository
+{
+    IEnumerable<AccountRole> GetAll();
+    AccountRole? GetByGuid(Guid id);
+    AccountRole? Create(AccountRole accountRole);
+    bool Update(AccountRole accountRole);
+    bool Delete(AccountRole accountRole);
+}
+
